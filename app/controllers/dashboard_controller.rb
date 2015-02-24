e1f3750824f845c1bfd1ae8778c30a64a9cef285
Matt Hamrick <mrhamric@gmail.com>
2015-02-23 19:55:26 -0700

    show and hide a  list of reports

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
   def index
     @report = Report.last
     @should_go = Report.last.yesterday_snow_total > 0
+    @recent_reports = Report.all
   end
 
   private
