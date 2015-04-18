745e86b5981344b74f37dc967ed158806c26bd14
Matt Hamrick <mrhamric@gmail.com>
2015-04-17 19:35:19 -0600

    remove more stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,18 +4,3 @@
 //= require react
 //= require react_ujs
 //= require_tree .
-
-var ready;
-
-ready = function() {
-  _toggleReports();
-};
-
-_toggleReports = function() {
-  $('#report_link').click(function () {
-    $('.report_list').toggle();
-  });
-};
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
