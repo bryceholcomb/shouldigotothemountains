b6ecc713aaa7d85cf42050187b16c04702341849
Matt Hamrick <mrhamric@gmail.com>
2015-04-19 13:23:43 -0400

    rm comment

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,6 @@ class DashboardController < ApplicationController
   respond_to :json, :html
 
   def index
-
-    # Give me some love! Better should_i_go logic, explodes if no reports present
-
     @recent_reports = Report.all.order(yesterday_snow_total: :desc)
     @report = @recent_reports.first
     @should_go = @report.yesterday_snow_total > 3
